d68b344f3837a87a73cb01d4cc7e434d6f583d41
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-12-29 19:20:27 -0500

    Modal working

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+/**
+ * Created by aryanjabbari on 12/29/15.
+ */
+$(window).load(function() {
+    $('#choosePlayer').modal({
+        backdrop: 'static',
+        show: true,
+        keyboard: false
+    });
+});
